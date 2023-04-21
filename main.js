9a954a79d5cb21fe609354f2412dc89f836926b5
sean <seanyi433@gmail.com>
2023-04-20 21:22:41 -0400

    labs

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const selectAllHearts = document.querySelectorAll(".like-glyph")
 
+for(let loop of selectAllHearts){
+  loop.addEventListener('click', mockmimicServerCall)
+}
+
+const modal = document.querySelector('#modal')
+modal.className = "hidden"
+function mockmimicServerCall(e){
+mimicServerCall()
+  .then(function(){
+    console.log(e.target)
+    if(e.target.innerText === EMPTY_HEART){
+    e.target.className = 'activated-heart'
+    e.target.innerText = FULL_HEART}
+
+    else{
+      e.target.className = ''
+      e.target.innerText = EMPTY_HEART
+    }
+  })
+.catch(function(error){
+ modal.className = ""
+ modal.innerText = error
+  setTimeout(function(){
+    modal.className = "hidden"
+  }, 3000)
+})
+}
 
 
 
